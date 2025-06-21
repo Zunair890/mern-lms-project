09b62e4b9ecd85a556067a85fb60b0b73d3c2cc1
Zunair Ali <ranazunairali007@gmail.com>
2025-06-21 17:10:55 +0500

    activate user

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -64,6 +64,43 @@ interface IActivationToken{
 export const createActivationToken= (user:any): IActivationToken=>{
     const activationCode= Math.floor(1000* Math.random()*9000).toString();
 
-    const token= jwt.sign({user,activationCode},process.env.ACTIVATION_SECRET,{expiresIn:"1d"} );
+    const token= jwt.sign({user,activationCode},process.env.ACTIVATION_SECRET,{expiresIn:"10min"} );
     return {token,activationCode}
 }
+
+
+// activate user
+
+interface IActivationRequest{
+    activation_token: string,
+    activation_code: string
+}
+
+export const activateUser= catchAsyncError(async(req:Request,res:Response,next:NextFunction)=>{
+    try {
+        const {activation_token,activation_code}=req.body as IActivationRequest;
+
+        const newUser: { user: IUser; activationCode: string } = jwt.verify(
+            activation_token,
+            process.env.ACTIVATION_SECRET as string
+          ) as { user: IUser; activationCode: string };
+      
+        if(newUser.activationCode !== activation_code){
+            return next(new ErrorHandler("Invalid activation code",400));
+        }
+
+        const {name,email,password}= newUser.user;
+        const existingUser= await userModel.findOne({email});
+        if (existingUser) {
+            return next(new ErrorHandler(' Email already exist', 400));
+          }
+      
+          const user = await userModel.create({ email, password, name });
+          res.status(200).json({ message:"success: true" });
+
+
+
+    } catch (error) {
+        return next( new ErrorHandler("Invalid activation code",400));
+    }
+})
