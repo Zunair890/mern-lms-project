78102e76b3ac0b1683587b13807553d1dc5cca3c
Zunair Ali <ranazunairali007@gmail.com>
2025-06-23 17:31:16 +0500

    edit course data

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -3,6 +3,7 @@ import { catchAsyncError } from "../utils/catchAsyncError";
 import { NextFunction, Request, Response } from "express";
 import ErrorHandler from "../utils/ErrorHandler";
 import { createCourse } from "../services/course.services";
+import courseModel from "../models/course.models";
 
 
 // uploading the course
@@ -26,5 +27,39 @@ export const uploadCourse= catchAsyncError(async(req:Request,res:Response,next:N
 
     } catch (error) {
         return next(new ErrorHandler(error,400));
+    }
+});
+
+
+// edit the course
+
+export const editCourse= catchAsyncError(async(req:Request,res:Response,next:NextFunction)=>{
+    try {
+        const data= req.body;
+        const thumbnail= data.thumbnail;
+        if(thumbnail){
+            await cloudinary.uploader.destroy(thumbnail.public_id);
+            const myCloud= await cloudinary.uploader.upload(thumbnail,{
+                folder:"courses"
+            });
+
+            data.thumbnail={
+                public_id: myCloud.public_id,
+                url: myCloud.secure_url
+            }
+        }
+
+        const courseId= req.params.id;
+        const course= await courseModel.findByIdAndUpdate(courseId,{
+            $set:data
+        },{
+            new: true
+        })
+        res.status(201).json({
+            success: true,
+            course
+        })
+    } catch (error) {
+        return next(new ErrorHandler(error.message,400));
     }
 })
